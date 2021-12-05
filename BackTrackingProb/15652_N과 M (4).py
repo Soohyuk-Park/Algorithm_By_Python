71cb4d8af755288f650fad5710cdbec8d888b800
Soohyuk-Park <95116100+Soohyuk-Park@users.noreply.github.com>
2021-12-05 20:27:12 +0900

    Add files via upload

---
+++ BackTrackingProb/15652_N과 M (4).py
@@ -0,0 +1,23 @@
+## https://www.acmicpc.net/problem/15652 ##
+
+n,m = map(int,input().split())
+
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str,s)))
+        return
+
+    for i in range(1, n+1):
+        if s == []:
+            s.append(i)
+        elif s[-1] <= i:
+            s.append(i)
+        else:
+            continue
+        dfs()
+        s.pop()
+
+
+dfs()
