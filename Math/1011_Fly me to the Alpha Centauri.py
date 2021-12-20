06897e4f63eb133c4aa1e63c2ff289e70b2c0ca5
Soohyuk-Park <95116100+Soohyuk-Park@users.noreply.github.com>
2021-12-20 19:58:47 +0900

    Add files via upload

---
+++ Math/1011_Fly me to the Alpha Centauri.py
@@ -0,0 +1,17 @@
+#https://www.acmicpc.net/problem/1011
+# 코드 자체보다 수학적인 규칙이 헷갈렸던 문제.. 22 33 444 555.. 이런거
+
+t = int(input())
+
+for _ in range(t):
+    x, y = map(int,input().split())
+    distance = y - x
+    count = 0  # 이동 횟수
+    move = 1  # count별 이동 가능한 거리
+    move_plus = 0  # 이동한 거리의 합
+    while move_plus < distance :
+        count += 1
+        move_plus += move  # count 수에 해당하는 move를 더함
+        if count % 2 == 0 :  # count가 2의 배수일 때,
+            move += 1
+    print(count)
