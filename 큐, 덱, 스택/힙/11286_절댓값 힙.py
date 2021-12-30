ee7ddac1a38e7c57b626db3453e04c2ef76e2074
Soohyuk-Park <95116100+Soohyuk-Park@users.noreply.github.com>
2021-12-31 08:31:31 +0900

    Add files via upload

---
+++ 큐, 덱, 스택/힙/11286_절댓값 힙.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/11286
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+arr = []
+
+for _ in range(n):
+    k = int(input())
+    arr.append(k)
+
+for i in range(len(arr)):
+    n = arr[i]
+    if( n != 0):
+        heapq.heappush(q,(abs(n),(n)))
+    else:
+        if(not q):
+            print(0)
+        else:
+            x,y = (heapq.heappop(q))
+            print(y)
