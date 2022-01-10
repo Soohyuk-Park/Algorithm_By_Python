e0017880c10703504d907d254ac0e54f39a07aca
Soohyuk-Park <95116100+Soohyuk-Park@users.noreply.github.com>
2022-01-10 09:00:34 +0900

    Update 1759_암호 만들기.py

--- Brute Force/1759_암호 만들기.py
+++ Brute Force/1759_암호 만들기.py
@@ -1,3 +1,4 @@
+## https://www.acmicpc.net/problem/1759
 from itertools import combinations
 length, num = map(int,input().split())
 L = list(map(ord,input().split()))
@@ -14,3 +15,23 @@ for i in combinations(L,length):
       for k in range(len(i)):
             print(i[k],end='')
       print()
+    
+##################
+from itertools import combinations
+
+L, C = map(int, input().split())
+candidate = input().split()
+
+vowel = [x for x in candidate if x in ['a', 'e', 'i', 'o', 'u']]
+others = [x for x in candidate if x not in ['a', 'e', 'i', 'o', 'u']]
+
+ans = []
+for i in range(1, len(vowel) + 1):
+    for a in combinations(vowel, i):
+        if L - i >= 2:
+            for b in combinations(others, L - i):
+                ans.append(sorted(a + b))
+
+ans.sort()
+for s in ans:
+    print("".join(s))
